0ca980018e5402a0ed114f915db6bdede18eb49e
Jason Spitzak <jason.spitzak@gmail.com>
2022-04-01 11:34:51 -0400

    added coloring

---
+++ 204x63.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# this is an april fools prank that is run from the terminal, just run it from the terminal
+# it looks just like a real terminal (you will have to change the username) but whenever anything
+# is typed in it just says command not found and prints april fools with the command tree 
+# a few things to note:
+# 1.
+#   control c or quitting the terminal will end the program
+# 2. 
+#   in a real terminal if the command kjfi eurg was entered it would return command not found kjfi
+#   this is because even if the command can't be found it treats eurg as an argument, my program does not
+# 3.
+#   my cover story:
+#   my cover story for my target (Jeff Elkner, my cs teacher) is that this has happened before and 
+#   quitting it stopped it but I want to experiment with it more so he should not quit the session
+#   also that there was one command that worked (tree) but I won't remember it until a minute in
+#if anybody actually uses this other than me I would love to see it
+#please record and send a video to jason.spitzak@gmail.com
+#||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+clear
+
+RED='\033[0;31m'
+PURPLE='\033[1;35m'
+BLUE='\033[0;34m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+while :
+do
+
+    read -p "1002864@1002864-student-FVFG349LQ6L3 ~ % " INPUT 
+
+
+    if [[ $INPUT == 'tree' ]]
+    then
+        #echo APRIL FOOLS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        echo "${RED}APRIL ${PURPLE}FOOLS${BLUE}!!!!!!!!!!!!!!!!!!${GREEN}!!!!!!!!!!!!!!!!!!!!!!${NC}"
+    else
+        echo zsh: command not found: $INPUT
+    fi
+
+
+done
