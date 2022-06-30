fc5e9e3c725da99535449e718e4b52be5b5875ac
J-Spitzak <jason.spitzak@gmail.com>
2022-06-29 21:47:06 -0400

    removed unintentionally duplicated file, got bird sim to actually work and added a little anotation

--- bird_sim.py
+++ bird_sim.py
@@ -1,13 +1,13 @@
 class NCurve():
 
-    def __init__(self, number = None, mean = None, zScore = None, standard_deviation = None):
+    def __init__(self, number = None, mean = None, standard_deviation = None, zScore = None):
         self.num = number
         self.zScore = zScore
         self.mean = mean
         self.stddev = standard_deviation
 
     def Zscore(self):
-        pass
+        return (self.num - self.mean) / self.stddev 
 
 class population():
 
@@ -17,19 +17,25 @@ class population():
         self.dependencies = []
 
     def dependencies_calc(self):
+        #print(self.dependencies)
         final = 0
         dp_type_score = []
         importance = []
         for dependencyType in self.dependencies:
             score = 0
             for dependency in dependencyType[0]:
-                score += dependency[0].population.zScore * dependency[1]
+                score += dependency[0].population.Zscore() * dependency[1]
+                #print(dependency[0].population.Zscore() , dependency[1])
             importance.append(dependencyType[1])
+            #print(score, dependencyType[1])
             dp_type_score.append(score)
         for i in range(len(dp_type_score)):
             final += dp_type_score[i] * importance[i]
+        #print(dp_type_score)
+        #print(importance)
         return final
 
 birds = population(500, 400, 50)
-birds.dependencies = [[[[population(20,20,2), 5], [population(120,100,30),2]],5],[[[population(15,20,5),2], [population(500,300,150),3]],5]]
+birds.dependencies = [[[[population(10,20,2), 5], [population(120,100,30),2]],5],[[[population(15,20,5),2], [population(500,300,150),3]],5]]
+#            specific population ^ importance ^        importance of resource ^   all populations that contribute to resource end here ^
 print(birds.dependencies_calc())
