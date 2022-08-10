dcccbbc27bfacfae361418c381f79435c4026d8f
J-Spitzak <jason.spitzak@gmail.com>
2022-08-10 16:40:59 -0400

    fixed edge case where inverted vector of particle didn't clear it of it's inversion range

--- physics.py
+++ physics.py
@@ -24,30 +24,58 @@ def invert(list):
 
 class particle:
     def __init__(self, x, y, width):
-        self.x = x
-        self.y = y
-        self.g_force = [0,0]
+        self.x_right = x
+        self.y_bottom = y
+
+        self.g_force = [.1,0]
         self.final_force = [-0.3,.2]
+        self.final_force = [0,0]
+
         self.width = width
+
+        self.x_left = self.x_right - self.width
+        self.y_top = self.y_bottom - self.width
 
     def vec_add(self):
         self.final_force[0] += self.g_force[0]
         self.final_force[1] += self.g_force[1]
 
     def walls(self):
-        if self.y <= Y_T or self.y >= Y_B:
+        '''if self.y <= Y_T or self.y >= Y_B:
             self.final_force[1] *= -1
         if self.x <= X_L or self.x >= X_R:
+            self.final_force[0] *= -1'''
+        # <- for walls
+
+        if self.y_bottom >= SCREEN_HEIGHT or self.y_top <= 0:
+            self.final_force[1] *= -1
+
+            if self.y_top + self.final_force[1] <= 0:
+                self.y_bottom = 1 + self.width
+
+            if self.y_bottom + self.final_force[1] >= SCREEN_HEIGHT:
+                self.y_bottom = SCREEN_HEIGHT - 1
+            
+        if self.x_left <= 0 or self.x_right >= SCREEN_WIDTH:
             self.final_force[0] *= -1
+
+            if self.x_left + self.final_force[0] <= 0:
+                self.x_right = 1 + self.width
+
+            if self.x_right + self.final_force[0] >= SCREEN_WIDTH:
+                self.x_right = SCREEN_WIDTH - 1
+
     
     def move(self):
-        self.x += self.final_force[0]
-        self.y += self.final_force[1]
+        self.x_right += self.final_force[0]
+        self.y_bottom += self.final_force[1]
+
+        self.x_left = self.x_right - self.width
+        self.y_top = self.y_bottom - self.width
     
     def draw(self):
-        x_pos = self.x - self.width
-        y_pos = self.y - self.width
-        pg.draw.rect(WIN, (255, 225, 225), pg.Rect(x_pos, y_pos, self.width, self.width))
+        
+        pg.draw.rect(WIN, (255, 225, 225), pg.Rect(self.x_left, self.y_top, self.width, self.width))
         
     
 par = particle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 5)
@@ -73,7 +101,7 @@ def stuffs():
 
         if ticks == 10:
             WIN.fill(BLACK)
-            draw_square()
+            #draw_square()
             main()
             par.draw()
             pg.display.update()
