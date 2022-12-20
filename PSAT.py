67337aeb2d4cd3327abd5b883b89243302644e50
Jason Spitzak <jason.spitzak@gmail.com>
2022-12-20 16:38:28 -0500

    did stuff with PSAT practice script

--- PSAT.py
+++ PSAT.py
@@ -1,10 +1,12 @@
 import time
+timeLeft = 60*int(input("total time in minutes:"))
+totalMin = timeLeft / 60
+totalQ = int(input("total questions"))
+tpq = timeLeft/totalQ
+last_elaps = 0
 input("press enter to start")
 start = time.time()
-timeLeft = 60*25
-tpq = timeLeft/17
-last_elaps = 0
-for _ in range(17):
+for _ in range(totalQ):
     input()
     elps_frm_strt = time.time() - start
     diff = elps_frm_strt - last_elaps
@@ -18,8 +20,8 @@ for _ in range(17):
     last_elaps = elps_frm_strt
 print("done")
 thisTime = time.time() - start
-print("you finished in", thisTime, "seconds, the time neccesary was", 60*25)
-if thisTime > 60*25:
-    print("you were too slow by", thisTime - 60*25)
+print("you finished in", thisTime, "seconds, the time neccesary was", 60*totalMin)
+if thisTime > 60*totalMin:
+    print("you were too slow by", thisTime - 60*totalMin)
 else :
-    print("you were", 60*25 - thisTime, "seconds ahead of time")
+    print("you were", 60*totalMin - thisTime, "seconds ahead of time")
